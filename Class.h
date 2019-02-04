29aad8100c034ac884e431fa5af594f512a391cd
181014muzamil <46879735+181014muzamil@users.noreply.github.com>
2019-02-04 10:22:58 +0500

    Add files via upload

---
+++ Class.h
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle
+{
+int width,lenght;
+public:
+int getwidth();
+int getlenght();
+void setwidth(int);
+void setlenght(int);
+int getarea();
+void rotateRectangle();
+void drawRectangle();
+};
