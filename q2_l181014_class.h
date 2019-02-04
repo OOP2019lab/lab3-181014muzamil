bae14bd5f51d7c940fba885c199e2146e9a4b80c
181014muzamil <46879735+181014muzamil@users.noreply.github.com>
2019-02-04 10:52:51 +0500

    Add files via upload

---
+++ q2_l181014_class.h
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+class Rectangle
+{
+int width,lenght;
+public:
+	void setlenght(int);
+	void setwidth(int);
+	int getlenght();
+	int getwidth();
+	int getarea();
+	void rotateRectangle();
+	void drawRectangle();
+};
---
+++ q2_l181014_test.cpp
@@ -0,0 +1,16 @@
+#include "Header.h";
+
+
+int main(){
+	Rectangle A;
+	int lenght=0,width=0;
+	A.setwidth(width);
+	A.setlenght(lenght);
+	int m=A.getwidth();
+	int n=A.getlenght();
+	cout<<A.getarea()<<endl;
+	A.drawRectangle();
+	A.rotateRectangle();
+	cout<<A.getarea()<<endl;
+	A.drawRectangle();
+}
